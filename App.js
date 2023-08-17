8c3069d77da22e5e9754c6fd25905e24f7a224e0
unknown <22114290012@ACADEMICO.IESB.BR>
2023-08-16 21:33:50 -0300

    Primeiro

--- App.js
+++ App.js
@@ -1,20 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+//São os imports coisas normais
 
 export default function App() {
+  //Tudo que for retornar na tela tem que ser na view
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Tray again!</Text>
+      
+      <View style={styles.imageIcon}>
+        <Image style={styles.imageIcon} source={require('./assets/favicon.png')} />
+      </View>
+      
+      
+      
     </View>
   );
 }
 
+//Aqui fica o styles que é a estilização da page e ele é chamado lá em cima
+//Está sendo citado também dentro do views
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  imageIcon: {
+    paddingTop: 10,
   },
 });
