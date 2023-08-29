578cb4e1b49c37f08b2e57e545654e9ba228cea7
unknown <22114290012@academico.iesb.br>
2023-08-29 19:49:48 -0300

    Exercicio

---
+++ components/Atividade.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+//Atalho para criar a estrutura do arquivo é rnfs
+export default function Atividade() {
+  return (
+    <View>
+      <Text>Atividade</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
